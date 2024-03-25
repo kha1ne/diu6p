06832bea3fcbe27a384c167b5d5bb821e69848f9
Marko Maric <mmaric@gmail.com>
2024-03-25 15:52:41 +0100

    Fix manifest.json

--- src/DropdownPlayers.js
+++ src/DropdownPlayers.js
@@ -1,6 +1,13 @@
 import React from "react";
 import { Select, MenuItem, FormControl } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "./DropdownPlayers.css";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Anta"].join(","),
+  },
+});
 
 function DropdownPlayers({
   playersList,
@@ -8,27 +15,34 @@ function DropdownPlayers({
   handleDropdownChange,
 }) {
   return (
-    <div className="dropdowns">
-      {selectedPlayers.map((player, index) => (
-        <FormControl key={index} fullWidth>
-          <Select
-            value={player}
-            onChange={(e) => handleDropdownChange(index, e.target.value)}
-            displayEmpty
-            style={{ minWidth: "200px", marginBottom: "20px" }}
-          >
-            <MenuItem value="Select player" disabled>
-              Select player
-            </MenuItem>
-            {playersList.map((option, i) => (
-              <MenuItem key={i} value={option}>
-                {option}
+    <ThemeProvider theme={theme}>
+      <div className="dropdowns">
+        {selectedPlayers.map((player, index) => (
+          <FormControl key={index} fullWidth>
+            <Select
+              value={player}
+              onChange={(e) => {
+                return handleDropdownChange(index, e.target.value);
+              }}
+              displayEmpty
+              style={{
+                width: "300px",
+                marginBottom: "20px",
+              }}
+            >
+              <MenuItem value="Select player" disabled>
+                Select player
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-      ))}
-    </div>
+              {playersList.map((option, i) => (
+                <MenuItem key={i} value={option}>
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        ))}
+      </div>
+    </ThemeProvider>
   );
 }
 
