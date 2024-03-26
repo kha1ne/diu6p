0cd955a9561b10120b52046afc255409ec7cb38f
Marko Maric <mmaric@gmail.com>
2024-03-26 14:19:40 +0100

    Remove theme from everywhere

--- src/CheckboxAssignLeaders.js
+++ src/CheckboxAssignLeaders.js
@@ -1,27 +1,18 @@
 import React from "react";
 import { FormControlLabel, Checkbox } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({
-  typography: {
-    fontFamily: ["Anta"].join(","),
-  },
-});
 
 function CheckboxAssignLeaders({ assignLeaders, handleAssignLeadersChanged }) {
   return (
-    <ThemeProvider theme={theme}>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={assignLeaders}
-            onChange={handleAssignLeadersChanged}
-            color="primary"
-          />
-        }
-        label="Randomly Assign Allies' Leaders"
-      />
-    </ThemeProvider>
+    <FormControlLabel
+      control={
+        <Checkbox
+          checked={assignLeaders}
+          onChange={handleAssignLeadersChanged}
+          color="primary"
+        />
+      }
+      label="Randomly Assign Allies' Leaders"
+    />
   );
 }
 
