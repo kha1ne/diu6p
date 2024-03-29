6fd6f6559099495f53ae26a18dd921e3532d4280
Marko Maric <mmaric@gmail.com>
2024-03-29 15:36:50 +0100

    adding player slot

--- src/DropdownPlayers.js
+++ src/DropdownPlayers.js
@@ -1,42 +1,62 @@
 import { FormControl, MenuItem, Select } from '@mui/material';
 import React from 'react';
+import questionmark from './img/questionmark.png';
 
 function DropdownPlayers({
   playersList,
   selectedPlayers,
   handleDropdownChange
 }) {
+  const combinedWidthPercentage = 55;
+
   return (
-    <div>
+    <div
+      style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}
+    >
       {selectedPlayers.map((player, index) => (
-        <FormControl key={index} fullWidth>
-          <Select
-            value={player}
-            onChange={e => {
-              return handleDropdownChange(index, e.target.value);
-            }}
+        <div
+          key={index}
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            marginBottom: '15px',
+            width: `${combinedWidthPercentage}vmin`,
+            justifyContent: 'center'
+          }}
+        >
+          <img
+            src={questionmark}
+            alt={`Player ${index + 1}`}
             style={{
-              marginRight: 'auto',
-              marginLeft: 'auto',
-              marginTop: 'auto',
-              marginBottom: '15px',
-              width: '45vmin'
+              marginRight: '10px',
+              width: 'auto',
+              height: '54'
             }}
-          >
-            <MenuItem value="Select player" disabled>
-              Select player
-            </MenuItem>
-            {playersList.map((option, i) => (
-              <MenuItem
-                key={i}
-                value={option}
-                disabled={selectedPlayers.includes(option) && option !== player}
-              >
-                {option}
+          />
+          <FormControl fullWidth style={{ flex: 1 }}>
+            <Select
+              value={player}
+              onChange={e => {
+                return handleDropdownChange(index, e.target.value);
+              }}
+            >
+              <MenuItem value="Select player" disabled>
+                Select player
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+              {playersList.map((option, i) => (
+                <MenuItem
+                  key={i}
+                  value={option}
+                  disabled={
+                    selectedPlayers.includes(option) && option !== player
+                  }
+                >
+                  {option}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
       ))}
     </div>
   );
