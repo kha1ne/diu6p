eac750d45d1d8d9b0de9f99010b01171cd24c847
Marko Maric <mmaric@gmail.com>
2026-08-22 12:16:21 +0200

    Add kill-switch service worker at legacy CRA path

---
+++ public/service-worker.ts
@@ -0,0 +1,9 @@
+// Kill switch: unregisters this stale CRA service worker then reloads
+self.addEventListener('install', () => {
+  void self.skipWaiting();
+});
+self.addEventListener('activate', () => {
+  void self.registration.unregister().then(() => self.clients.matchAll()).then(clients => {
+    clients.forEach(client => void client.navigate(client.url));
+  });
+});
