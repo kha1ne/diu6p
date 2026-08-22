a429f7fc1a55805c46b0fc4d94c10f91a9d4335a
Marko Maric <mmaric@gmail.com>
2026-08-22 12:09:12 +0200

    Unregister stale CRA service worker on load

--- src/pwa.ts
+++ src/pwa.ts
@@ -2,6 +2,18 @@ import { registerSW } from 'virtual:pwa-register';
 
 import { logger } from './utils';
 
+// Unregister stale CRA service worker left from pre-migration deployment
+if ('serviceWorker' in navigator) {
+  void navigator.serviceWorker.getRegistrations().then(registrations => {
+    for (const reg of registrations) {
+      if (reg.active?.scriptURL.includes('service-worker.ts')) {
+        void reg.unregister();
+        logger.info('Unregistered stale CRA service worker');
+      }
+    }
+  });
+}
+
 if (import.meta.env.PROD) {
   registerSW({
     immediate: true,
